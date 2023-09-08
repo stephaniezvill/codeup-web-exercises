38f93e12be760487dee2fb0813a76e4710dbde6f
StephanieVillarreal <stephanie_zoz_villarreal@gmail.com>
2023-09-08 12:04:27 -0500

    added new html along with .js files

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log('Hello from external JavaScript!');
+
+alert("Welcome to my Website!");
+
+let userChoice = prompt ("What is your favorite color?");
+
+alert(`Great, ${userChoice} is my favorite color too!`)
--- js/javascript-with-html.js
+++ js/javascript-with-html.js
@@ -10,7 +10,6 @@ let myNumber = 7;
 
 // let userChoice = confirm("Wait! Don't leave yet! Can I chat with you!");
 //
-//
 // if (userChoice === true){
 //     console.log("yay!");
 // } else {
@@ -18,3 +17,15 @@ let myNumber = 7;
 // }
 
 /*==== PROMPT =======*/
+
+let currentSpeed = prompt("How fast are you driving?");
+let plannedSpeed = prompt("How fast do you want to drive?");
+let distanceLeft = prompt("How many miles do you have left?");
+
+let timeAtCurrentSpeed = (distanceLeft/currentSpeed) * 60;
+
+let timeAtPlannedSpeed = (distanceLeft/plannedSpeed) * 60;
+
+let timeSaved = timeAtCurrentSpeed - timeAtPlannedSpeed;
+
+console.log(`At your current speed you will take ${timeAtCurrentSpeed.toFixed(0)} minutes. At your planned speed you will take ${timeAtPlannedSpeed.toFixed(0)} minutes. You will save ${timeSaved} minutes. `);
