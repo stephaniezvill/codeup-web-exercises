87b7d74a93d862e95e032363fbc5d83d79f2e75b
StephanieVillarreal <stephanie_zoz_villarreal@gmail.com>
2023-09-08 10:23:17 -0500

    cover alert and prompt

---
+++ js/javascript-with-html.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let myNumber = 7;
+
+/*==== ALERT =======*/
+
+//alert("Hello, I am an ALERT!");
+
+/*==== PROMPT =======*/
+
+// let userChoice = confirm("Wait! Don't leave yet! Can I chat with you!");
+//
+//
+// if (userChoice === true){
+//     console.log("yay!");
+// } else {
+//     console.log("boo");
+// }
