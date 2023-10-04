bf56a66e77152f45412a833cba0efe8b9680fce6
stephaniezvill <stephanie.zoz.villarreal@gmail.com>
2023-10-03 21:37:20 -0500

    Completed Exercise on Flexbox Nav

--- js/for-loops.js
+++ js/for-loops.js
@@ -5,31 +5,31 @@ function getRandomInt(min,max) {
 function isEven(num) {
     return num % 2 === 0;
 }
-// Number 2
-// function showMultiplicationTable (num) {
-//     for(let i = 1; i <= 10; i++) {
-//         console.log( `${num} x ${i} = ${num * i}` );
-//  }
-// }
-// showMultiplicationTable(7)
+Number 2
+function showMultiplicationTable (num) {
+    for(let i = 1; i <= 10; i++) {
+        console.log( `${num} x ${i} = ${num * i}` );
+ }
+}
+showMultiplicationTable(7)
 
-// Number 3
-// for (let i= 0; i <= 10; i++) {
-//     let number= getRandomInt (20, 200);
-//         console.log (`The number is: ${number}`);
-//         console.log(`Number is even: ${isEven(number)}`)
-//     }
+Number 3
+for (let i= 0; i <= 10; i++) {
+    let number= getRandomInt (20, 200);
+        console.log (`The number is: ${number}`);
+        console.log(`Number is even: ${isEven(number)}`)
+    }
 
-// Number 4
-// for (let row = 1; row <=9; row++) {
-// let str=`${row}`;
-//
-// for (let j = 1; j<row ; j++)){
-// str += row;
-// }
-// console.log(str);
+Number 4
+for (let row = 1; row <=9; row++) {
+let str=`${row}`;
 
-//Number 5
-// for (let i = 100; i >= 5; i-=5) {
-//     console.log(i)
-// }
+for (let j = 1; j<row ; j++)){
+str += row;
+}
+console.log(str);
+
+Number 5
+for (let i = 100; i >= 5; i-=5) {
+    console.log(i)
+}
